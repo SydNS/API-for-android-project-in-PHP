8bf7e760de8f5cad56d8df4849c1cd957fe8238b
SydNS <sydneynsiimbe@gmail.com>
2021-01-27 18:37:01 -0800

    create a sql file for creating the necessary database and related tables

---
+++ db.sql
@@ -0,0 +1,3 @@
+CREATE TABLE `phprestcrud`.`posts` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `category_id` INT(11) NOT NULL , `title` VARCHAR(255) NOT NULL , `body` TEXT NOT NULL , `author` VARCHAR(255) NOT NULL , `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+CREATE TABLE `phprestcrud`.`categories` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `created_at` DATETIME NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
