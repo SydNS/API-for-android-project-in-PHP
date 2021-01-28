a9d8c90b854df0e2200594b6a24d34ece297c303
SydNS <sydneynsiimbe@gmail.com>
2021-01-28 01:36:29 -0800

    created another function  for reading a single record using an id

--- models/Post.php
+++ models/Post.php
@@ -39,6 +39,44 @@ class Post{
           
      }
 
+    //  read single
+     public function readSingles()
+     {
+         $query="SELECT
+         c.name as category_name,
+         p.id,
+         p.category_id,
+         p.title, 
+         p.body,
+         p.category_id,
+         p.author,
+         p.created_at
+         from posts p
+          LEFT JOIN 
+            categories c ON p.category_id=c.id
+          ORDER BY
+            p.created_at DESC
+          where
+          p.id =? 
+          limit 0,1  ";
+
+          $stmt= $this->conn->prepare($query);
+          $stmt->bindParams(1,$this->id);
+          $stmt->execute();
+
+          $row= $this->fetch(PDO::FETCH_SSOC);
+          $this->title=$row['title'];
+          $this->body=$row['body'];
+          $this->author=$row['author'];
+          $this->category=$row['category'];
+          $this->category_name=$row['category_name'];
+
+
+
+          return $stmt;
+          
+     }
+
 
 
 }
