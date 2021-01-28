9be940258a858cf17311609a7de9df8425134cdd
SydNS <sydneynsiimbe@gmail.com>
2021-01-27 17:21:54 -0800

    created a configuration file for the project

---
+++ Database.php
@@ -0,0 +1,23 @@
+<?php
+class Database{
+    private $host='localhost'
+    private $db_name='phprestcrud'
+    private $username='root'
+    private $password=''
+    private $conn='localhost'
+
+ public function connect()
+ {
+     $this->conn = null;
+     try{
+
+        $this->conn=new PDO('mysql:host='.$this->host .';dbname='. $this->db_name,$this->username.$this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODe_EXCEPTION);
+
+     }catch{
+         echo 'Connection Error' .$e->getMessage();
+     }
+
+     return $this->conn;
+ }
+}
