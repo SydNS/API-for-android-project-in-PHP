0720ebfbb403c098a0a3c3f367b95cd5c5c267fe
SydNS <sydneynsiimbe@gmail.com>
2021-01-28 00:16:38 -0800

    added the database password and removed the exception

--- config/Database.php
+++ config/Database.php
@@ -1,17 +1,17 @@
 <?php
 class Database{
     private $host='localhost';
-    private $db_name='phprestcrud';
-    private $username='root';
-    private $password='';
-    private $conn='localhost';
+    private $db_name="phprestcrud";
+    private $username="root";
+    private $password ="root";
+    private $conn;
 
  public function connect()
  {
      $this->conn = null;
      try{
-         $this->conn=new PDO('mysql:host='.$this->host .';dbname='. $this->db_name,$this->username.$this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODe_EXCEPTION);
+         $this->conn=new PDO('mysql:host='. $this->host .';dbname='. $this->db_name,$this->username,$this->password );
+        // $this->conn->setAttribute(PDO::ATTR_ERROR_MODE,PDO::ERRMODE_EXCEPTION);
 
      }catch(PDOException $e){
          echo 'Connection Error' .$e->getMessage();
