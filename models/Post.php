259e40daab48b5feb39ffae0bf2627e913a85269
SydNS <sydneynsiimbe@gmail.com>
2021-01-28 14:35:08 -0800

    Removed the order by on the readSingle() function because we are returning a single record added

--- models/Post.php
+++ models/Post.php
@@ -40,7 +40,7 @@ class Post{
      }
 
     //  read single
-     public function readSingles()
+    public function readSingle()
      {
          $query="SELECT
          c.name as category_name,
@@ -54,21 +54,20 @@ class Post{
          from posts p
           LEFT JOIN 
             categories c ON p.category_id=c.id
-          ORDER BY
-            p.created_at DESC
           where
           p.id =? 
-          limit 0,1  ";
+          limit 1  ";
 
           $stmt= $this->conn->prepare($query);
-          $stmt->bindParams(1,$this->id);
+          $stmt->bindParam(1,$this->id);
           $stmt->execute();
 
-          $row= $this->fetch(PDO::FETCH_SSOC);
+          $row= $stmt->fetch(PDO::FETCH_ASSOC);
+
           $this->title=$row['title'];
           $this->body=$row['body'];
           $this->author=$row['author'];
-          $this->category=$row['category'];
+          $this->category_id=$row['category_id'];
           $this->category_name=$row['category_name'];
 
 
@@ -77,6 +76,42 @@ class Post{
           
      }
 
+
+     public function inSert()
+     {
+         $query="INSERT INTO posts
+          
+         SET
+          title = :title, 
+          body = :body, 
+          age = :age, 
+          author = :author, 
+          category_id=category_id,
+          created_at = :created_at";
+
+          
+        $stmt= $this->conn->prepare($query);
+
+        $this->title=htmlspecialchars(strip_tags($this->title));
+        $this->body=htmlspecialchars(strip_tags($this->body));
+        $this->author=htmlspecialchars(strip_tags($this->author));
+        $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+
+        $stmt->bindParam(":title", $this->title);
+        $stmt->bindParam(":body", $this->body);
+        $stmt->bindParam(":age", $this->age);
+        $stmt->bindParam(":author", $this->author);
+        $stmt->bindParam(":category_id", $this->category_id);
+        
+        if ($stmt->execute()) {
+            return true;
+            # code...
+        }
+        printf("Error : %s  .\n",$stmt->error)
+          
+     }
+
+     
 
 
 }
